7ed7ce4d09cea2502a55ca9a46a68e7e28ce8bbf
GroovyPeng <aleksey.mischanchuk@gmail.com>
2022-07-23 00:13:55 +0300

    project alfa

---
+++ src/main/java/my/projects/reviewSite/controllers/MainController.java
@@ -0,0 +1,28 @@
+package my.projects.reviewSite.controllers;
+
+import lombok.AllArgsConstructor;
+import my.projects.reviewSite.services.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class MainController {
+    final private PostService postService;
+
+    @GetMapping("/")
+    public String Main(Model model) {
+        model.addAttribute("posts", postService.findAllPosts());
+        model.addAttribute("title", "Review Site");
+        return "main";
+    }
+
+    @PostMapping("/")
+    public String deletePost(@RequestParam Long id) {
+        postService.deletePost(id);
+        return "redirect:/";
+    }
+}
---
+++ src/test/java/my/projects/reviewSite/ReviewSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.reviewSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
