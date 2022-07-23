25fb64b1646ae5f1e46838e168732fc4c3b6ebe5
GroovyPeng <aleksey.mischanchuk@gmail.com>
2022-07-23 20:17:57 +0300

    l

--- src/main/java/my/projects/reviewSite/repositories/ThemeRepo.java
+++
@@ -1,7 +0,0 @@
-package my.projects.reviewSite.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.ui.context.Theme;
-
-public interface ThemeRepo extends CrudRepository<Theme, String> {
-}
