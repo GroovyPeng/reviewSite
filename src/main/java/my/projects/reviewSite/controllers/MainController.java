6a24a378ef21e31c4bcc7a753ac1a24ba061d4ae
GroovyPeng <aleksey.mischanchuk@gmail.com>
2022-07-23 14:35:28 +0300

    a

--- src/main/java/my/projects/reviewSite/controllers/MainController.java
+++ src/main/java/my/projects/reviewSite/controllers/MainController.java
@@ -1,11 +1,14 @@
 package my.projects.reviewSite.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
+
     @GetMapping("/")
     public String Main(Model model) {
         model.addAttribute("title", "Review Site");
