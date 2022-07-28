6adc2f14211367cf06482da0b5a202740dfd248c
GroovyPeng <aleksey.mischanchuk@gmail.com>
2022-07-28 17:48:46 +0300

    Updating Date
    Filling date in existing posts

--- src/main/java/my/projects/reviewSite/controllers/MainController.java
+++ src/main/java/my/projects/reviewSite/controllers/MainController.java
@@ -1,12 +1,15 @@
 package my.projects.reviewSite.controllers;
 
 import lombok.AllArgsConstructor;
+import my.projects.reviewSite.models.Post;
 import my.projects.reviewSite.services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 @AllArgsConstructor
@@ -15,6 +18,11 @@ public class MainController {
 
     @GetMapping("/")
     public String Main(Model model) {
+        for (Post post : postService.findAllPosts()) {
+            post.setDate(new Date());
+            postService.savePost(post);
+        }
+
         model.addAttribute("posts", postService.findAllPosts());
         model.addAttribute("title", "Review Site");
         return "main";
