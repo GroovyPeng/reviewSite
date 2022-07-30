287827a2038015dde2706a62067cdc0641179b44
GroovyPeng <aleksey.mischanchuk@gmail.com>
2022-07-30 22:53:10 +0300

    Adding politics on delete button & some fixes
    - Add delete permition to only authorized users;
    - Rename 'title' on user profile;
    - Add user entity.

--- src/main/java/my/projects/reviewSite/controllers/ProfileController.java
+++ src/main/java/my/projects/reviewSite/controllers/ProfileController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ProfileController {
 
     @GetMapping("/profile")
-    public String profilePage(
-            Model model
-    ) {
-        model.addAttribute("title", "Профиль");
+    public String profilePage(Model model) {
+        model.addAttribute("title", "Личный кабинет");
         return "profile";
     }
 }
